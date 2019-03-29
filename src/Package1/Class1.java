9725d65edd30dcfeb3a6b0722c17a0b5f33223e1
kalyanihandal <kalyanihandal28@gmail.com>
2019-03-29 14:49:53 +0530

    Uploading my local project to Git at AccessModifiers

---
+++ src/Package1/Class1.java
@@ -0,0 +1,15 @@
+package Package1;
+
+public class Class1 {
+public int a1i=10;
+static int x=30;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Class1 a1=new Class1();
+Class1 a2=new Class1();
+a1.x=a1.x+5;
+a2.x=a2.x+5;
+System.out.println(Class1.x);
+	}
+
+}
